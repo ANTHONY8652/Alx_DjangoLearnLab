50dd828732cf899215ca55d8e8ea8296d04a1e07
ANTHONY8652 <githinjianthony720@gmail.com>
2024-08-16 17:53:58 +0300

    Second last commit wink wink

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -3,5 +3,39 @@ from .views import list_books, LibraryDetailView
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail')
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
+
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from .views.register  # Import the register view
+
+
+urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', register, name='register'),
+]
+
+from django.urls import path
+from .views import admin_view, librarian_view, member_view
+
+urlpatterns = [
+    path('admin/', admin_view, name='admin_view'),
+    path('librarian/', librarian_view, name='librarian_view'),
+    path('member/', member_view, name='member_view'),
+]
+
+from django.urls import path
+from .views import add_book, edit_book, delete_book
+
+urlpatterns = [
+    path('add_book/', add_book, name='add_book'),  # Notice the underscore
+    path('edit_book/<int:book_id>/', edit_book, name='edit_book'),  # Notice the underscore
+    path('delete_book/<int:book_id>/', delete_book, name='delete_book'),
+]
+
+]
+
+
+ 
