edaf600b542563c41b108e1b5f63e37391ca79ed
ANTHONY8652 <githinjianthony720@gmail.com>
2024-08-16 17:26:16 +0300

    believe in yourself

---
+++ django-models/relationship_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import list_books, LibraryDetailView
+
+urlpatterns = [
+    path('books/', list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail')
+]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Book, Library
+from django.views.generic.detail import DetailView
+
+#Lists all books stored in tehe database
+def list_books(request):
+    books = Book.objects.all()
+    context = {'books': books}
+    return render(request, 'list_books.html', context)
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
 
 # Create your views here.
