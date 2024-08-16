4cef776f280067512c362a08083d0f15390a887e
ANTHONY8652 <githinjianthony720@gmail.com>
2024-08-16 16:24:49 +0300

    Third commit wink wink

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,28 +1,19 @@
-from django.db import models
-from .models import Author, Book, Library, Librarian
+from relationship_app.models import Author, Book, Library, Librarian
 
-def query_books_by_author(author_name):
+#Query all books mby a specific author
+def books_by_author(author_name):
     author = Author.objects.get(name=author_name)
-    return author.books.all()
+    books = Book.objects.filter(author=author)
+    return books
 
+#List all books in a library
 def list_books_in_library(library_name):
     library = Library.objects.get(name=library_name)
-    return library.books.all()
+    books = library.books.all()
+    return books
 
+#Get librarian for a limbrary
 def get_librarian_for_library(library_name):
     library = Library.objects.get(name=library_name)
-    return library.librarian
-
-if __name__ == "__main__":
-
-    #Query all books by a specific author
-    author_books = query_books_by_author("J.K Rowling")
-    print("Books by J.K Rowling:", [book.title for book in author_books])
-
-    #List all books in a library
-    library_books = list_books_in_library("Central Library")
-    print("Books in Central Library:", [book.title for book in library_books])
-
-    #retrieve the librarian for a library
-    librarian = get_librarian_for_library("Central Library")
-    print("Librarian of Central Library:" librarian.name if librarian else "No librarian assigned")
+    librarian = Librarian.objects.get(library=library)
+    return librarian
