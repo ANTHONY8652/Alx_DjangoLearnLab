78982dfed0257cc58ec95acc83fc2acf7f5f0368
ANTHONY8652 <githinjianthony720@gmail.com>
2024-08-22 17:52:13 +0300

    Third of many

--- advanced_features_and_security/relationship_app/models.py
+++ advanced_features_and_security/relationship_app/models.py
@@ -1,4 +1,3 @@
-from datetime import date
 from django.db import models
 
 # Create your models here.
@@ -30,7 +29,7 @@ class Librarian(models.Model):
         return self.name
 
 from django.db import models
-from django.contrib.auth.models import User, AbstractUser, BaseUserManager
+from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     ROLE_CHOICES = (
@@ -68,45 +67,3 @@ class Book(models.Model):
             ("can_change_book", "Can change a book"),
             ("can_delete_book", "Can delete a book"),
         ]
-
-
-class CustomUser(AbstractUser):
-    date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
-
-    objects = CustomUserManager()
-
-    def __str__(self):
-        return self.username
-    
-class CustomUserManager(BaseUserManager):
-    def create_user(self, email, username, date_of_birth, password=None, **extra_fields):
-        if not email:
-            raise ValueError('The Email field must be set')
-        email = self.normalize_email(email)
-        user = self.model(email=email, username=username, date_of_birth=date_of_birth, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-    
-    def create_superuser(self, email, username, date_of_birth, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True. ')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True. ')
-        
-        return self.create_user(email, username, date_of_birth, password, **extra_fields)
-
-
-from django.conf import settings
-
-class ExampleModel(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-
-from django.conf import settings
-
-class ExampleModel(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
