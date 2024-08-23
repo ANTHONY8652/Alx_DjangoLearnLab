50c7966a3cf4919c3acdac10eb2789329be3a7c2
ANTHONY8652 <githinjianthony720@gmail.com>
2024-08-23 18:09:37 +0300

    Final go

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
 from .models import Post, Book, MyModel
+from .forms import ExampleForm
 from django.http import HttpResponse
 from django import forms
 
