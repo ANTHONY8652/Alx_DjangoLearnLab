829e72671ee6a867e5940421bdf214a796eef0e9
ANTHONY8652 <githinjianthony720@gmail.com>
2024-08-31 16:38:03 +0300

    Setting up a new Django Project with Django REST Framework

---
+++ api_project/api/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    description = models.TextField()
+    
+    def __str__(self):
+        return self.title
+
+# Create your models here.
