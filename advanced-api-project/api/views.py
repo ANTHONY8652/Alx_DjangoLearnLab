b857a4ffd63440e57306979b38f93cfe94b58730
ANTHONY8652 <githinjianthony720@gmail.com>
2024-09-07 18:52:10 +0300

    Implementing, filtering, Searching and Ordering

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,6 +4,7 @@ from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework
 
 #LIst all boooks
 #Book list view with filtering
