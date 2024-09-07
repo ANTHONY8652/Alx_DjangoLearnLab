867906b7544d8ded7f7d5709ed7236c4b95d2335
ANTHONY8652 <githinjianthony720@gmail.com>
2024-09-07 19:13:59 +0300

    Writing unit tests

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -14,8 +14,8 @@ class BookAPITests(APITestCase):
         # Create test data
         self.book1 = Book.objects.create(title="Book One", author="Author A", publication_year=2020)
         self.book2 = Book.objects.create(title="Book Two", author="Author B", publication_year=2021)
-        self.list_url = reverse('book-list')  # Adjust with your actual URL name
-        self.detail_url = lambda pk: reverse('book-detail', kwargs={'pk': pk})  # Detail view URL
+        self.list_url = reverse('book-list')
+        self.detail_url = lambda pk: reverse('book-detail', kwargs={'pk': pk})
 
     def test_create_book(self):
         data = {
@@ -31,7 +31,7 @@ class BookAPITests(APITestCase):
     def test_get_books(self):
         response = self.client.get(self.list_url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 2)  # Verify the number of books returned
+        self.assertEqual(len(response.data), 2) #Verify the number of books returned
 
     def test_update_book(self):
         data = {
