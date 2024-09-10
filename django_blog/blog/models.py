0287f1c611ade98baaea6e8d9b5edd0b8c4b3c39
ANTHONY8652 <githinjianthony720@gmail.com>
2024-09-10 12:59:13 +0300

    Initial Setup and Project Configuration

---
+++ django_blog/blog/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name = 'posts')
+
+# Create your models here.
